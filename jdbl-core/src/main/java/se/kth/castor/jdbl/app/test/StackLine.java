a3de36a297602df55cf4b0ee3a9780434aa8255d
tdurieux <durieuxthomas@hotmail.com>
2020-05-25 13:34:20 +0200

    add method to extract stacktrace class and methods

---
+++ jdbl-core/src/main/java/se/kth/castor/jdbl/app/test/StackLine.java
@@ -0,0 +1,59 @@
+package se.kth.castor.jdbl.app.test;
+
+import java.util.Objects;
+
+public class StackLine {
+    private String className;
+    private String method;
+    private String file;
+    private int line;
+
+    public StackLine(String className, String method, String file, int line) {
+        this.className = className;
+        this.method = method;
+        this.file = file;
+        this.line = line;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StackLine stackLine = (StackLine) o;
+        return line == stackLine.line &&
+                Objects.equals(className, stackLine.className) &&
+                Objects.equals(method, stackLine.method) &&
+                Objects.equals(file, stackLine.file);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(className, method, file, line);
+    }
+
+    @Override
+    public String toString() {
+        return "StackLine{" +
+                "className='" + className + '\'' +
+                ", method='" + method + '\'' +
+                ", file='" + file + '\'' +
+                ", line=" + line +
+                '}';
+    }
+}
--- jdbl-core/src/main/java/se/kth/castor/jdbl/app/test/TestResultReader.java
+++ jdbl-core/src/main/java/se/kth/castor/jdbl/app/test/TestResultReader.java
@@ -1,8 +1,12 @@
 package se.kth.castor.jdbl.app.test;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -16,9 +20,65 @@ public class TestResultReader
         this.path = path;
     }
 
+    private static Set<StackLine> extractError(String content) {
+        Set<StackLine> output = new HashSet<>();
+        String[] lines = content.split("\n");
+        for (int i = 0; i < lines.length; i++) {
+            String line = lines[i].trim();
+            if (!line.startsWith("at ")) {
+                continue;
+            }
+            int index = line.indexOf('(');
+            if (index == -1) {
+                continue;
+            }
+
+            String classMethodName = line.substring(3, index);
+            int indexMethod = classMethodName.lastIndexOf('.');
+            String methodName = classMethodName.substring(indexMethod + 1);
+            String className = classMethodName.substring(0, indexMethod);
+            String[] position = line.substring(index + 1, line.length() - 2).split(":");
+            StackLine stackLine = new StackLine(className, methodName, position[0], Integer.parseInt(position[1]));
+            output.add(stackLine);
+        }
+        return output;
+    }
+    public Set<StackLine> getMethodFromStackTrace() {
+        Set<StackLine> output = new HashSet<>();
+        File file = new File(path + "/target/surefire-reports");
+        if (!file.exists()) {
+            file = new File(path);
+        }
+        if (!file.exists()) {
+            return output;
+        }
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        for (File f : file.listFiles()) {
+            if (f.getName().endsWith(".xml")) {
+                try {
+                    DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                    Document doc = dBuilder.parse(f);
+                    doc.getDocumentElement().normalize();
+                    NodeList errors = doc.getDocumentElement().getElementsByTagName("error");
+                    for (int i = 0; i < errors.getLength(); i++) {
+                        Node error = errors.item(i);
+                        output.addAll(extractError(error.getTextContent()));
+                    }
+                    errors = doc.getDocumentElement().getElementsByTagName("rerunError");
+                    for (int i = 0; i < errors.getLength(); i++) {
+                        Node error = errors.item(i);
+                        output.addAll(extractError(error.getTextContent()));
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return output;
+    }
+
     public TestResult getResults()
     {
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         File file = new File(path + "/target/surefire-reports");
         if (!file.exists()) {
             file = new File(path);
@@ -26,6 +86,7 @@ public class TestResultReader
         if (!file.exists()) {
             return null;
         }
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         int nbErrors = 0;
         int nbFailure = 0;
         int nbTest = 0;
