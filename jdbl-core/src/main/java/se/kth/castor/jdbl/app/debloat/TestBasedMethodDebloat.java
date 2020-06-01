1eeb8b8724c209cc5dcb805c6e51df14a886c6f2
César Soto Valero <cesarsotovalero@gmail.com>
2020-06-01 16:41:53 +0200

    Do not remove one line methods to fix https://github.com/castor-software/jdbl/issues/9

--- jdbl-core/src/main/java/se/kth/castor/jdbl/app/debloat/TestBasedMethodDebloat.java
+++ jdbl-core/src/main/java/se/kth/castor/jdbl/app/debloat/TestBasedMethodDebloat.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
-import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
 import java.util.Set;
@@ -18,93 +17,111 @@ import org.apache.log4j.Logger;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.LineNumberNode;
 import org.objectweb.asm.tree.MethodNode;
+
 import se.kth.castor.jdbl.app.test.StackLine;
 
 public class TestBasedMethodDebloat extends AbstractMethodDebloat
 {
-   protected static final Logger LOGGER = LogManager.getLogger(TestBasedMethodDebloat.class);
-   private final Set<StackLine> failingMethods;
+    protected static final Logger LOGGER = LogManager.getLogger(TestBasedMethodDebloat.class);
+    private final Set<StackLine> failingMethods;
 
-   public TestBasedMethodDebloat(String outputDirectory, Map<String, Set<String>> usageAnalysis, File reportFile, Set<StackLine> failingMethods)
-   {
-      super(outputDirectory, usageAnalysis, reportFile);
-      this.failingMethods = failingMethods;
-   }
+    public TestBasedMethodDebloat(String outputDirectory, Map<String, Set<String>> usageAnalysis, File reportFile, Set<StackLine> failingMethods)
+    {
+        super(outputDirectory, usageAnalysis, reportFile);
+        this.failingMethods = failingMethods;
+    }
 
-   @Override
-   public void removeMethod(String clazz, Set<String> unusedMethods) throws IOException
-   {
-      FileInputStream in = new FileInputStream(new File(outputDirectory + "/" + clazz + ".class"));
-      ClassReader cr = new ClassReader(in);
+    @Override
+    public void removeMethod(String clazz, Set<String> unusedMethods) throws IOException
+    {
+        FileInputStream in = new FileInputStream(new File(outputDirectory + "/" + clazz + ".class"));
+        ClassReader cr = new ClassReader(in);
 
-      Set<StackLine> methods = new HashSet<>();
-      for (StackLine failingMethod : failingMethods) {
-         if (failingMethod.getClassName().equals(clazz.replace("/", "."))) {
-            methods.add(failingMethod);
-         }
-      }
-      ClassNode clNode = new ClassNode(Opcodes.ASM8);
-      cr.accept(clNode, Opcodes.ASM8);
-      for (MethodNode mNode: clNode.methods) {
-         for (StackLine failingMethod : methods) {
-            if (mNode.name.equals(failingMethod.getMethod())) {
-               ListIterator<AbstractInsnNode> it = mNode.instructions.iterator();
-               while (it.hasNext()) {
-                  AbstractInsnNode inNode = it.next();
-                  if (inNode instanceof LineNumberNode) {
-                     if (((LineNumberNode) inNode).line == failingMethod.getLine()) {
-                        unusedMethods.remove(mNode.name + mNode.desc);
-                     }
-                  }
-               }
+        Set<StackLine> methods = new HashSet<>();
+        for (StackLine failingMethod : failingMethods) {
+            if (failingMethod.getClassName().equals(clazz.replace("/", "."))) {
+                methods.add(failingMethod);
             }
-         }
-      }
+        }
+        ClassNode clNode = new ClassNode(Opcodes.ASM8);
+        cr.accept(clNode, Opcodes.ASM8);
+        for (MethodNode mNode : clNode.methods) {
 
-      ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_MAXS);
-      ClassVisitor cv = new ClassVisitor(Opcodes.ASM8, cw)
-      {
-         @Override
-         public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions)
-         {
-            MethodVisitor mv = cv.visitMethod(access, name, desc, signature, exceptions);
+            ignoreOneLineMethods(unusedMethods, mNode);
 
-            if (unusedMethods.contains(name + desc)) {
-               LOGGER.info("Removed method: " + name + desc + " in " + clazz);
-               // write report to file
-               writeReportToFile(name, desc, "BloatedMethod, ", clazz);
-               return new MethodExceptionThrower(mv);
-               // return null;
-            } else {
-               // write report to file
-               writeReportToFile(name, desc, "UsedMethod, ", clazz);
+            for (StackLine failingMethod : methods) {
+                if (mNode.name.equals(failingMethod.getMethod())) {
+                    ListIterator<AbstractInsnNode> it = mNode.instructions.iterator();
+                    while (it.hasNext()) {
+                        AbstractInsnNode inNode = it.next();
+                        if (inNode instanceof LineNumberNode) {
+                            if (((LineNumberNode) inNode).line == failingMethod.getLine()) {
+                                unusedMethods.remove(mNode.name + mNode.desc);
+                            }
+                        }
+                    }
+                }
             }
-            return mv;
-            // return super.visitMethod(access, name, desc, signature, exceptions);
-         }
-      };
-      cr.accept(cv, ClassReader.SKIP_DEBUG);
-      byte[] code = cw.toByteArray();
-      try (OutputStream fos = new FileOutputStream(outputDirectory + "/" + clazz.replace(".", "/") + ".class")) {
-         fos.write(code);
-      } catch (Exception ignored) {
-         LOGGER.error("Error replacing class " + clazz + " with debloated methods ");
-      }
-   }
+        }
 
-   private void writeReportToFile(final String name, final String desc, final String usageType, String clazz)
-   {
-      try {
-         FileUtils.writeStringToFile(reportFile, usageType + clazz + ":" + name + desc + "\n", StandardCharsets.UTF_8, true);
-      } catch (IOException e) {
-         LOGGER.error("Error writing the methods report.");
-      }
-   }
+        ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_MAXS);
+        ClassVisitor cv = new ClassVisitor(Opcodes.ASM8, cw)
+        {
+            @Override
+            public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions)
+            {
+                MethodVisitor mv = cv.visitMethod(access, name, desc, signature, exceptions);
+
+                if (unusedMethods.contains(name + desc)) {
+                    LOGGER.info("Removed method: " + name + desc + " in " + clazz);
+                    // write report to file
+                    writeReportToFile(name, desc, "BloatedMethod, ", clazz);
+                    return new MethodExceptionThrower(mv);
+                    // return null;
+                } else {
+                    // write report to file
+                    writeReportToFile(name, desc, "UsedMethod, ", clazz);
+                }
+                return mv;
+                // return super.visitMethod(access, name, desc, signature, exceptions);
+            }
+        };
+        cr.accept(cv, ClassReader.SKIP_DEBUG);
+        byte[] code = cw.toByteArray();
+        try (OutputStream fos = new FileOutputStream(outputDirectory + "/" + clazz.replace(".", "/") + ".class")) {
+            fos.write(code);
+        } catch (Exception ignored) {
+            LOGGER.error("Error replacing class " + clazz + " with debloated methods ");
+        }
+    }
+
+    private void ignoreOneLineMethods(final Set<String> unusedMethods, final MethodNode mNode)
+    {
+        ListIterator<AbstractInsnNode> iter = mNode.instructions.iterator();
+        int counter = 0;
+        while (iter.hasNext()) {
+            AbstractInsnNode inNode = iter.next();
+            if (inNode instanceof LineNumberNode) {
+                counter++;
+            }
+        }
+        if (counter <= 1) {
+            unusedMethods.remove(mNode.name + mNode.desc);
+        }
+    }
+
+    private void writeReportToFile(final String name, final String desc, final String usageType, String clazz)
+    {
+        try {
+            FileUtils.writeStringToFile(reportFile, usageType + clazz + ":" + name + desc + "\n", StandardCharsets.UTF_8, true);
+        } catch (IOException e) {
+            LOGGER.error("Error writing the methods report.");
+        }
+    }
 }
