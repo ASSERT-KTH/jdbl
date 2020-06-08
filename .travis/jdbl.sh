6e9842a5df3835c692e8fc5d96ade6656e10da68
César Soto Valero <cesarsotovalero@gmail.com>
2020-06-09 00:07:06 +0200

    Fix test

--- .travis/jdbl.sh
+++ .travis/jdbl.sh
@@ -2,7 +2,7 @@
 
 mvn clean install -Ptravis
 mvn jacoco:report coveralls:report --fail-never
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.coverage.jacoco.xmlReportPaths=jdbl-core/target/site/jacoco/jacoco.xml -Dsonar.projectKey=castor-software_jdbl
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.coverage.jacoco.xmlReportPaths=jdbl-core/target/site/jacoco/jacoco.xml,jdbl-maven-plugin/target/site/jacoco/jacoco.xml -Dsonar.projectKey=castor-software_jdbl
 
 
 
